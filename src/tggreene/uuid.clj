84dba9962571de64aa532941eda29f633c9be58c
Tim Greene <tim.g.greene@gmail.com>
2020-06-07 10:26:24 +0100

    Removed unused functions

--- src/tggreene/uuid.clj
+++ src/tggreene/uuid.clj
@@ -1,23 +1,6 @@
 (ns tggreene.uuid
   (:import [java.util UUID]))
 
-(defn bit-slice
-  "Not (particularly) related to [bit-slicing](https://en.wikipedia.org/wiki/Bit_slicing)"
-  [x start n]
-  (assert (number? x) "x must be a number")
-  (bit-and (bit-shift-right  x (- start n)) (dec (long (Math/pow 2 n)))))
-
-(defn uuid->str
-  [uuid]
-  (let [most  (.getMostSignificantBits uuid)
-        least (.getLeastSignificantBits uuid)]
-    (format "%08x-%04x-%04x-%04x-%012x"
-            (bit-slice most 64 32)
-            (bit-slice most 32 16)
-            (bit-slice most 16 16)
-            (bit-slice least 64 16)
-            (bit-slice least 48 48))))
-
 (def gregorian-to-epoch-seconds 122192928000000000)
 
 (defn generate-v6
@@ -33,13 +16,3 @@
                      (bit-and 0x7FFFFFFF00000000 (bit-shift-left time 4))
                      (bit-and 0x000000000FFFFFFF time))]
     (UUID. most least)))
-
-(defn is-uuid-v6?
-  [^UUID u]
-  (java.lang.ByteBuffer. (.getMostSignficantBits u) (.getLeastSignificantBits u)))
-
-(comment
-  (java.nio.ByteBuffer)
-  (bytes (rand-int 10))
-  (type (.getMostSignificantBits (UUID/randomUUID)))
-  (generate-v6))
